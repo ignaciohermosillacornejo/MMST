d2d1779009f734376eb4e689e8120a94a6d840d5
antonio <alopez7@uc.cl>
2018-06-18 20:43:13 -0400

    Listo repositorio

---
+++ Programa/src/solver/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Modo de uso: ./solver test.txt\n");
+		return 0;
+	}
+
+	// Retorno ok
+	return 0;
+}
